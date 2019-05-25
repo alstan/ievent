00838c25139a6afccfa9c41adeaa8f77a29db382
stanley albuquerque <stanleyaraujo2@gmail.com>
2019-05-25 14:35:58 -0300

    movies

--- server/db.js
+++ server/db.js
@@ -4,6 +4,11 @@ module.exports = () => ({
       id: 1,
       name: 'Aladin',
       imageSrc: 'https://ingresso-a.akamaihd.net/img/cinema/cartaz/14744-cartaz.jpg'
+    },
+    {
+      id: 2,
+      name: 'Hellboy',
+      imageSrc: 'https://ingresso-a.akamaihd.net/img/cinema/cartaz/22053-cartaz.jpg'
     }
   ]
 }) 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,27 +3,28 @@ import { Card } from 'antd';
 import { getMovies } from '../services';
 
 class Movies extends Component {
-
   state = {
     movies: []
   }
 
   async componentDidMount() {
-    console.log(await getMovies());
+    this.setState({ movies: await getMovies() });
   }
 
   render() {
+    const { movies } = this.state;
+
     return (<Fragment>
-      {/*movies.map(movie => 
+      {movies.map(movie => 
         <Card
+          key={movie.id}
           hoverable
           style={{ width: 240 }}
-          cover={<img alt="example" src="https://ingresso-a.akamaihd.net/img/cinema/cartaz/14744-cartaz.jpg" />}
+          cover={<img alt="example" src={movie.imageSrc} />}
         >
-          <h4>Aladin</h4>
+          <h4>{movie.name}</h4>
         </Card>
-      )*/}
-      
+      )}
     </Fragment>);
   }
 }
